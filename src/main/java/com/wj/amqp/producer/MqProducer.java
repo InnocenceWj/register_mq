5c636f48cadecbe516bc93ea42e565f31c44fd45
wj <1575391348@qq.com>
2018-11-20 13:52:05 +0800

    【ampq】【constmq】

---
+++ src/main/java/com/wj/amqp/producer/MqProducer.java
@@ -0,0 +1,17 @@
+package com.wj.amqp.producer;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @创建人 wj
+ * @创建时间 2018/11/6
+ * @描述
+ */
+public interface MqProducer {
+    /**
+     * 发送消息到指定队列
+     * @param queueKey
+     * @param str
+     */
+    void sendDataToQueue(String exchangeKey,String queueKey, String str) throws UnsupportedEncodingException;
+}
---
+++ src/main/java/com/wj/constmq/Const.java
@@ -0,0 +1,13 @@
+package com.wj.constmq;
+
+/**
+ * @创建人 wj
+ * @创建时间 2018/11/2
+ * @描述
+ */
+public class Const {
+     public static final String EXCHANGE_NAME_MAIL = "email.exchange";
+     public static final String EXCHANGE_NAME_PHONE = "cellphone.exchange";
+     public static final String QUEUE_NAME_MAIL = "email";
+     public static final String QUEUE_NAME_PHONE = "cellphone";
+}
